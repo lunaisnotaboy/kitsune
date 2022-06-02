f02b6f947bba1032db9066988655f9c6798adfc1
Luna Nova <her@mint.lgbt>
2022-06-02 13:27:39 -0400

    Lots of stuff :3

---
+++ Pods/Target Support Files/Pods-Kitsune-KitsuneUITests/Pods-Kitsune-KitsuneUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Kitsune_KitsuneUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Kitsune_KitsuneUITests
+@end
---
+++ Pods/Target Support Files/Pods-Kitsune/Pods-Kitsune-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Kitsune : NSObject
+@end
+@implementation PodsDummy_Pods_Kitsune
+@end
